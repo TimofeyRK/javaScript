51f012534daf760dbdec189b0f794cd8be022928
Timofey Pereveziy <Black.Vice111122@gmail.com>
2018-02-14 00:25:28 +0200

    Radiobut. was added

--- js/maintwo.js
+++ js/maintwo.js
@@ -1,14 +1,28 @@
+// CHECKBOX
 
-function fun_one() {
-	var chbox;
-chbox=document.getElementById('one');
+// function fun_one() {
+// 	var chbox;
+// chbox=document.getElementById('one');
 
-if (chbox.checked) {
-	console.log('Выбран!')
+// if (chbox.checked) {
+// 	console.log('Выбран!')
+// }
+// else {
+// 	console.log('Не выбран!')
+// }
+
+// }
+
+
+
+
+// RADIOBUTTON
+
+function fun_two() {
+	var radio = document.getElementsByName('r1');
+	for (var i = 0; i < radio.length; i++) {
+		if (radio[i].checked) {
+			console.log("Выбран " + i + " элемент");
+		}
+	}
 }
-else {
-	console.log('Не выбран!')
-}
-
-}
-
